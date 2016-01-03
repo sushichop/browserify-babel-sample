d2cd1092a907ec7dbc1bd6eeeff2218466ed2971
sushichop <sushichop@users.noreply.github.com>
2016-01-03 20:57:55 +0900

    add plugins.babel()

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -17,6 +17,7 @@ gulp.task('scripts', () => {
     .pipe(source('app.js')) // file name or path to be build
     .pipe(buffer())
     .pipe(plugins.sourcemaps.init({loadMaps: true}))  // create source map
+    .pipe(plugins.babel())
     .pipe(plugins.uglify())
     .on('error', plugins.util.log)
     .pipe(plugins.sourcemaps.write('./'))
